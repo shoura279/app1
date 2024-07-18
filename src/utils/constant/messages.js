ebf83983d97f161dc32d96c670d1892d0ed294f7
shoura279 <ahmedshoura279@gmail.com>
2024-07-19 01:52:25 +0300

    feat: add constant messages

---
+++ src/utils/constant/messages.js
@@ -0,0 +1,16 @@
+const generateMessages = (entity) => ({
+    notFound: `${entity} not found`,
+    alreadyExist: `${entity} already exist`,
+    failToCreate: `fail to create ${entity}`,
+    failToUpdate: `fail to update ${entity}`,
+    createdSuccessfully: `${entity} created successfully`,
+    updatedSuccessfully: `${entity} updated successfully`,
+    deletedSuccessfully: `${entity} deleted successfully`
+})
+export const messages = {
+    category: generateMessages('category'),
+    subcategory: generateMessages('subcategory'),
+    brand: generateMessages('brand'),
+    product: generateMessages('product'),
+    user: generateMessages('user'),
+}
