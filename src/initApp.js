18068409b39a1aa2351f20d9f9b131baf4bc29d6
shoura279 <ahmedshoura279@gmail.com>
2024-07-21 22:21:00 +0300

    feat: add brand & product router

--- src/initApp.js
+++ src/initApp.js
@@ -1,5 +1,7 @@
 import { connectDB } from "../db/connection.js"
+import brandRouter from "./modules/brand/brand.router.js"
 import categoryRouter from "./modules/category/category.router.js"
+import productRouter from "./modules/product/product.router.js"
 import subcategoryRouter from "./modules/subcategory/subcategory.router.js"
 import { globalErrorHandling } from "./utils/asyncHandler.js"
 
@@ -12,6 +14,8 @@ export const initApp = (app, express) => {
     const port = process.env.PORT || 3000
     app.use('/category', categoryRouter)
     app.use('/sub-category', subcategoryRouter)
+    app.use('/brand', brandRouter)
+    app.use('/product', productRouter)
     app.use(globalErrorHandling)
     app.listen(port, () => console.log('server is running on port', port))
 }
